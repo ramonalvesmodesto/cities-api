d73d803757460ea13787f84df703c6d8e0249f48
Ramon9825 <ramonmodesto16@protonmail.com>
2021-05-07 22:24:14 -0300

    Primeiro commit

---
+++ src/main/java/com/github/Ramon9825/citiesapi/countries/repository/ContryRepository.java
@@ -0,0 +1,7 @@
+package com.github.Ramon9825.citiesapi.countries.repository;
+
+import com.github.Ramon9825.citiesapi.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContryRepository extends JpaRepository<Country, Long> {
+}
---
+++ src/main/java/com/github/Ramon9825/citiesapi/staties/StateRepository.java
@@ -0,0 +1,6 @@
+package com.github.Ramon9825.citiesapi.staties;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
